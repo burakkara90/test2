dbae89a4795e7d756c255719786c9a74dcc9f207
burak <burakkara0090@gmail.com>
2025-06-19 21:29:47 -0500

    gitsecondtest

---
+++ src/com/neotech/lesson1/homework.java
@@ -0,0 +1,18 @@
+package com.neotech.lesson1;
+
+public class homework {
+
+	public static void main(String[] args) {
+
+//		Create a java file and call it Homework1
+//		Print your "first name" space "last name" on the console.
+	
+		System.out.println("Anna Thurman");
+		
+		System.out.print("Anna");
+		
+		System.out.println(" Thurman");
+		
+	}
+
+}
---
+++ src/com/neotech/lesson1/homework2.java
@@ -0,0 +1,23 @@
+package com.neotech.lesson1;
+
+public class homework2 {
+
+	public static void main(String[] args) {
+	
+//		Create a java file and call it Homework 2
+//		Print your full name in one line
+//		Where you live in second line
+//		Print a blank line
+//		And your favorite color in fourth line.
+	
+			System.out.println("Berfin Avci");
+			
+			System.out.println("Toronto");
+		
+			System.out.println();
+			
+			System.out.println("black");
+			
+	}
+
+}
